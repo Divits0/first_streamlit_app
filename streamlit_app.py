90854ead9a476eef2ffc9964eb776e00871d05df
DIVIT SHAH <62374867+Divits0@users.noreply.github.com>
2023-02-13 10:31:57 +0530

    Update streamlit_app.py
    changed typo of title to text

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ streamlit.title("My parents new healthy diner.")
 streamlit.header("Breakfast Menu")
 
 streamlit.text("Omega 3 and blueberry oatmeal.")
-streamlit.title("Kale,Spinach and Rocket Smoothie.")
-streamlit.title("Hard-boiled Free ranged Egg.")
+streamlit.text("Kale,Spinach and Rocket Smoothie.")
+streamlit.text("Hard-boiled Free ranged Egg.")
 
 
