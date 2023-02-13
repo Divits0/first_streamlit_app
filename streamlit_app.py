6808881ad76e186c8e35531c5a9d66ba1645eb72
DIVIT SHAH <62374867+Divits0@users.noreply.github.com>
2023-02-13 10:48:45 +0530

    Update streamlit_app.py
    pre populated the list

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 import pandas
 
-streamlit.title("My parents new healthy diner.")
+streamlit.title("My Mom's new healthy diner.")
 streamlit.header("Breakfast Favorites")
 
 streamlit.text("🥣 Omega 3 and blueberry oatmeal.")
@@ -14,6 +14,6 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index),['Avocardo,Strawberries'])
 
 streamlit.dataframe(my_fruit_list)
