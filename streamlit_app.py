82376ac2a1571a697f2beb849a8f5e8a789ba76c
DIVIT SHAH <62374867+Divits0@users.noreply.github.com>
2023-02-13 10:35:14 +0530

    Update streamlit_app.py
    added emoji's and fourth menu item.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,11 @@
 import streamlit
 
 streamlit.title("My parents new healthy diner.")
-streamlit.header("Breakfast Menu")
+streamlit.header("Breakfast Favorites")
 
-streamlit.text("Omega 3 and blueberry oatmeal.")
-streamlit.text("Kale,Spinach and Rocket Smoothie.")
-streamlit.text("Hard-boiled Free ranged Egg.")
+streamlit.text("🥣 Omega 3 and blueberry oatmeal.")
+streamlit.text("🥗 Kale,Spinach and Rocket Smoothie.")
+streamlit.text("🐔 Hard-boiled Free ranged Egg.")
+streamlit.text("🥑🍞 Avocardo Toast.")
 
 
